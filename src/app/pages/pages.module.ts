8e1710fd93395271344cccb595fd9358f4ee67f5
Carlos <carlosmm147@gmail.com>
2019-01-02 18:15:42 -0600

    Se agregan colores de temas diferentes

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -21,6 +21,9 @@ import { IncrementadorComponent } from '../components/incrementador/incrementado
 import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
+
+
 
 
 
@@ -38,7 +41,8 @@ import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.co
         Graficas1Component,
         PagesComponent,
         IncrementadorComponent,
-        GraficoDonaComponent
+        GraficoDonaComponent,
+        AccountSettingsComponent
     ],
     exports: [
         DashboardComponent,
